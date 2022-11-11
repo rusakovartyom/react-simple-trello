c015ea814b42829c2c119a81df14847aff8d6319
Artyom Rusakov <popsninka@gmail.com>
2022-11-11 18:37:15 +0300

    Create List component

---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+import styles from './List.module.css';
+
+const List = () => {
+  return (
+    <div className={styles.List}>
+      <div className={styles.Title}>List Title</div>
+    </div>
+  );
+};
+export default List;
