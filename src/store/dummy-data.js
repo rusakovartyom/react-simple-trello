249054cd9eeef5b9e9e8309767b728745a883974
Artyom Rusakov <popsninka@gmail.com>
2022-11-14 14:37:51 +0300

    Refactor dummy data function (#11)

--- src/store/dummy-data.js
+++ src/store/dummy-data.js
@@ -4,79 +4,35 @@ import { listsActions } from './lists-slice';
 import { cardsActions } from './cards-slice';
 
 const dummyData = (store) => {
-  const firstListId = shortid.generate();
+  const dummyLists = {
+    'First List': ['First card', 'Second card'],
+    'Second List': ['Card 1', 'Card 2'],
+  };
 
-  store.dispatch(boardActions.addList({ listId: firstListId }));
-  store.dispatch(
-    listsActions.addList({ listId: firstListId, listTitle: 'First List' })
-  );
-
-  store.dispatch(
-    listsActions.addCard({
-      listId: firstListId,
-      cardId: shortid.generate(),
-    })
-  );
-
-  store.dispatch(
-    cardsActions.addCard({
-      listId: firstListId,
-      cardId: shortid.generate(),
-      cardText: 'First card',
-    })
-  );
-
-  store.dispatch(
-    listsActions.addCard({
-      listId: firstListId,
-      cardId: shortid.generate(),
-    })
-  );
-
-  store.dispatch(
-    cardsActions.addCard({
-      listId: firstListId,
-      cardId: shortid.generate(),
-      cardText: 'Second card',
-    })
-  );
-
-  const secondListId = shortid.generate();
-
-  store.dispatch(boardActions.addList({ listId: secondListId }));
-  store.dispatch(
-    listsActions.addList({ listId: secondListId, listTitle: 'Second List' })
-  );
-
-  store.dispatch(
-    listsActions.addCard({
-      listId: secondListId,
-      cardId: shortid.generate(),
-    })
-  );
-
-  store.dispatch(
-    cardsActions.addCard({
-      listId: secondListId,
-      cardId: shortid.generate(),
-      cardText: 'Card 1',
-    })
-  );
-
-  store.dispatch(
-    listsActions.addCard({
-      listId: secondListId,
-      cardId: shortid.generate(),
-    })
-  );
-
-  store.dispatch(
-    cardsActions.addCard({
-      listId: secondListId,
-      cardId: shortid.generate(),
-      cardText: 'Card 2',
-    })
-  );
+  const dispatchDummyData = (lists) => {
+    for (const list in lists) {
+      const listId = shortid.generate();
+      store.dispatch(boardActions.addList({ listId: listId }));
+      store.dispatch(listsActions.addList({ listId: listId, listTitle: list }));
+      for (const card of lists[list]) {
+        const cardId = shortid.generate();
+        store.dispatch(
+          listsActions.addCard({
+            listId: listId,
+            cardId: cardId,
+          })
+        );
+        store.dispatch(
+          cardsActions.addCard({
+            listId: listId,
+            cardId: cardId,
+            cardText: card,
+          })
+        );
+      }
+    }
+  };
+  dispatchDummyData(dummyLists);
 };
 
 export default dummyData;
