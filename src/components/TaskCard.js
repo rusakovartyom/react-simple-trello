79c5e7d14577aee89adf660c8aa865acd8fdb39f
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 18:39:33 +0300

    Edit and delete cards (#14)
    * Add props for mouse events
    
    * Add styling for card icons
    
    * Import usestate, create states for hover and editing
    
    * Add useDispatch hook
    
    * Add functions to toggle hover and editing states
    
    * Add handlers for editing and deleting cards
    
    * Rework component rendering
    
    * Add imports for reducers actions
    
    * Import styling for card icons

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,10 +1,72 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { cardsActions } from '../store/cards-slice';
+import { listsActions } from '../store/lists-slice';
 
 import Card from './Card';
+import CardEditor from './CardEditor';
+
+import styles from './TaskCard.module.css';
 
 const TaskCard = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const card = useSelector((state) => state.cardsById[props.cardId]);
+  const dispatch = useDispatch();
 
-  return <Card>{card.text}</Card>;
+  const startHover = () => {
+    setIsHovered(true);
+    console.log('Hovering.');
+  };
+  const endHover = () => {
+    setIsHovered(false);
+    console.log('Not hovering.');
+  };
+
+  const startEditing = () => {
+    setIsHovered(false);
+    setIsEditing(true);
+  };
+  const endEditing = () => {
+    setIsHovered(true);
+    setIsEditing(false);
+  };
+
+  const handleEditCard = async (text) => {
+    endEditing();
+
+    dispatch(cardsActions.changeCardText({ cardId: card._id, cardText: text }));
+  };
+
+  const handleDeleteCard = async () => {
+    const { listId, card } = props;
+
+    dispatch(cardsActions.deleteCard({ cardId: card._id, listId }));
+    dispatch(listsActions.deleteCard({ cardId: card._id, listId }));
+  };
+
+  if (!isEditing) {
+    return (
+      <Card onMouseEnter={startHover} onMouseLeave={endHover}>
+        {isHovered && (
+          <div className={styles.CardIcons}>
+            <div className={styles.CardIcon} onClick={startEditing}>
+              <ion-icon name="pencil" />
+            </div>
+          </div>
+        )}
+        {card.text}
+      </Card>
+    );
+  } else {
+    return (
+      <CardEditor
+        text={card.text}
+        onSave={handleEditCard}
+        onDelete={handleDeleteCard}
+        onCancel={endEditing}
+      />
+    );
+  }
 };
 export default TaskCard;
