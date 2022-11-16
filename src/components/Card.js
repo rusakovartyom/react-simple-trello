436a910651033c55e244e2734ea45e148187a539
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 18:16:10 +0300

    Add props for mouse events

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,14 @@
 import styles from './Card.module.css';
 
 const Card = (props) => {
-  return <div className={styles.Card}>{props.children}</div>;
+  return (
+    <div
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
+      className={styles.Card}
+    >
+      {props.children}
+    </div>
+  );
 };
 export default Card;
