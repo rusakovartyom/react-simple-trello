55d01f8471cafa5997cc08b07985d5e13cea0089
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 18:18:54 +0300

    Add useDispatch hook

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Card from './Card';
 
@@ -6,6 +7,7 @@ const TaskCard = (props) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const card = useSelector((state) => state.cardsById[props.cardId]);
+  const dispatch = useDispatch();
 
   return <Card>{card.text}</Card>;
 };
