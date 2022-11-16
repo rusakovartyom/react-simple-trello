402030c70b75d104ccb072850f6195a1c37ef05b
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 18:18:23 +0300

    Import usestate, create states for hover and editing

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,8 +1,10 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 import Card from './Card';
 
 const TaskCard = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const card = useSelector((state) => state.cardsById[props.cardId]);
 
   return <Card>{card.text}</Card>;
