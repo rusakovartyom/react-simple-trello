235b080332ed182647cd33ecfdefe224b67be406
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 12:31:15 +0300

    import useState, useDispatch, useSelector

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,29 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Card from './Card';
 
 import styles from './List.module.css';
 
 const List = (props) => {
+  const [addingCard, setAddingCard] = useState(false);
+  const dispatch = useDispatch();
   const list = useSelector((state) => state.listsById[props.listId]);
+
+  const toggleAddingCard = () => {
+    setAddingCard(!addingCard);
+  };
+
+  const handleAddCard = (cardText) => {
+    const listId = list._id;
+
+    toggleAddingCard();
+
+    const cardId = shortid.generate();
+
+    dispatch(listsActions.addCard({ cardId, listId }));
+    dispatch(cardsActions.addCard({ cardId, listId, cardText }));
+  };
 
   return (
     <div className={styles.List}>
