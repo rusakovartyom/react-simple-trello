81788f18a5f452f4967f8e860d92471eb4f0a419
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 15:26:23 +0300

    Add save, cancel and delete buttons

--- src/components/EditButtons.js
+++ src/components/EditButtons.js
@@ -2,6 +2,21 @@
 const EditButtons = (props) => {
   return (
     <div className={styles.EditButtons}>
+      <div
+        className={`${styles.EditButton} ${styles.EditButtonSave}`}
+        onClick={props.handleSave}
+      >
+        {props.saveLabel}
+      </div>
+      {props.handleDelete && (
+        <div
+          className={`${styles.EditButton} ${styles.EditButtonDelete}`}
+          onClick={props.handleDelete}
+        >
+          Delete
+        </div>
+      )}
+      <div className={styles.EditButtonCancel} onClick={props.handleCancel}>
     </div>
   );
 };
