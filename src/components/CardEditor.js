8f7de9b4a7034d52e3ef01b63105b799dfaa5a9f
Artyom Rusakov <popsninka@gmail.com>
2022-11-16 14:09:18 +0300

    Add state, handlers, TextAutosize component, EditButtons placeholder

--- src/components/CardEditor.js
+++ src/components/CardEditor.js
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 const CardEditor = (props) => {
+  const [text, setText] = useState('');
+
+  const handleChangeText = (event) => {
+    setText(event.target.value);
+  };
+
+  const onEnter = (e) => {
+    const { text } = props;
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      props.onSave(text);
+    }
+  };
+
   return (
+    <div className={styles.EditCard}>
+      <Card>
+        <TextareaAutosize
+          autoFocus
+          className={styles.EditCardTextarea}
+          placeholder="Enter the text for new card..."
+          value={text}
+          onChange={handleChangeText}
+          onKeyDown={onEnter}
+        />
+      </Card>
+      <div>Edit Buttons</div>
+    </div>
   );
 };
 export default CardEditor;
