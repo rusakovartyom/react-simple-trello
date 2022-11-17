2bfee411352efd7134b7a050476e1d66bd3d6660
Artyom Rusakov <popsninka@gmail.com>
2022-11-17 18:10:00 +0300

    Add custom hook to detect clicks outside of ListEditor

--- src/components/ListEditor.js
+++ src/components/ListEditor.js
@@ -1,12 +1,40 @@
+import { useEffect, useRef } from 'react';
 import styles from './ListEditor.module.css';
 
 const ListEditor = (props) => {
+  const ref = useRef();
+
   const onEnter = (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
       props.saveList();
     }
   };
+
+  // Custom hook for detecting click outside of the element
+  const useOnClickOutside = (ref, handler) => {
+    useEffect(() => {
+      const listener = (event) => {
+        // Do nothing if clicking ref's element or descendent elements
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+
+        handler(event);
+      };
+
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+      return () => {
+        document.removeEventListener('mousedown', listener);
+        document.removeEventListener('touchstart', listener);
+      };
+    }, [ref, handler]);
+  };
+
+  // Calling hook
+  useOnClickOutside(ref, () => props.onClickOutside(false));
+
   return (
   );
 };
