296f7ee92f078a166101c817b007dc34eecb2100
Artyom Rusakov <popsninka@gmail.com>
2022-11-17 15:33:11 +0300

    Add state for toggling between list editor and "add a list" button

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import List from './List';
@@ -5,6 +6,7 @@ import List from './List';
 import styles from './Board.module.css';
 
 const Board = () => {
+  const [isAddingList, setIsAddingList] = useState(false);
   const board = useSelector((state) => state.board);
 
   return (
