094a53898e47139f07d757b7a7cff5d1901694a8
Artyom Rusakov <popsninka@gmail.com>
2022-11-17 18:08:56 +0300

    Add function to save new list on enter

--- src/components/ListEditor.js
+++ src/components/ListEditor.js
@@ -1,6 +1,12 @@
 import styles from './ListEditor.module.css';
 
 const ListEditor = (props) => {
+  const onEnter = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      props.saveList();
+    }
+  };
   return (
   );
 };
