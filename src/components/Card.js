b2a79d7c3ce5c6c7ea4d972380374f6c6180f76a
Artyom Rusakov <popsninka@gmail.com>
2022-11-19 09:37:22 +0300

    Add props for drag and drop

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,9 @@ import styles from './Card.module.css';
 const Card = (props) => {
   return (
     <div
+      {...props.provided.draggableProps}
+      {...props.provided.dragHandleProps}
+      ref={props.innerRef}
       onMouseEnter={props.onMouseEnter}
       onMouseLeave={props.onMouseLeave}
       className={styles.Card}
